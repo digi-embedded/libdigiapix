b45a5b57dd8c261264649278b9d38ecf08fad368
David Escalona <david.escalona@digi.com>
2025-02-11 17:59:56 +0100

    cccs: python: update file headers
    Modify the CCCS APIx Python file headers to match the rest of the DEY Python
    APIx files.
    
    https://onedigi.atlassian.net/browse/OEMIOTD-235
    
    Signed-off-by: David Escalona <david.escalona@digi.com>

--- bindings/python/digi/apix/datapoint.py
+++ bindings/python/digi/apix/datapoint.py
@@ -3,30 +3,25 @@ APIs for uploading data points from the device
 
 This module enables the uploading of various data from
 the device:
-    from digidevice import datapoint
+    from digi.apix import datapoint
     datapoint.upload("mystreamid", "myvalue")
 
 Check help(datapoint.upload) for more details and parameters.
 """
-# Copyright 2018, 2021 Digi International Inc., All Rights Reserved
+# Copyright 2025, Digi International Inc.
 #
-# This software contains proprietary and confidential information of Digi
-# International Inc.  By accepting transfer of this copy, Recipient agrees
-# to retain this software in confidence, to prevent disclosure to others,
-# and to make no use of this software other than that for which it was
-# delivered.  This is an unpublished copyrighted work of Digi International
-# Inc.  Except as permitted by federal law, 17 USC 117, copying is strictly
-# prohibited.
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
-# Restricted Rights Legend
-#
-# Use, duplication, or disclosure by the Government is subject to
-# restrictions set forth in sub-paragraph (c)(1)(ii) of The Rights in
-# Technical Data and Computer Software clause at DFARS 252.227-7031 or
-# subparagraphs (c)(1) and (2) of the Commercial Computer Software -
-# Restricted Rights at 48 CFR 52.227-19, as applicable.
-#
-# Digi International Inc., 11001 Bren Road East, Minnetonka, MN 55343
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
 import csv
 import datetime
 import io
